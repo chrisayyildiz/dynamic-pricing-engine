63f1e2094b72cb116e00acc8133b61ea92a9b82a
Chris Ayyildiz <chrisayyildiz@outlook.com>
2025-05-20 12:02:02 +0000

    updates

--- backend/infrastructure/competitor_api.py
+++ backend/infrastructure/competitor_api.py
@@ -1,16 +1,8 @@
 import requests
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
-url = "https://google-flights2.p.rapidapi.com/api/v1/searchFlights"
+RAPID_KEY = os.getenv("RAPID_API_KEY")
 
-querystring = {"departure_id":"LHR","arrival_id":"ABZ","outbound_date":"2025-05-20","return_date":"2025-05-27","travel_class":"ECONOMY","adults":"1","show_hidden":"0","currency":"GBP","language_code":"en-US","country_code":"GB"}
-
-headers = {
-	"x-rapidapi-key": os.getenv("x-rapidapi-key"),
-	"x-rapidapi-host": "google-flights2.p.rapidapi.com"
-}
-
-response = requests.get(url, headers=headers, params=querystring)
-
-print(response.json())
-
+print(RAPID_KEY)
